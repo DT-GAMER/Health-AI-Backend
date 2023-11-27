af4985ce765f516557bc1988788ac25203df8d9b
Abakpa Dominic <abakpad82@gmail.com>
2023-11-27 13:35:39 +0100

    Update urls.py

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', views.login_user, name='login'),
-    path('accounts/', include('allauth.urls')),
 ]
 
